c4ec4d6de251f7d2622e61b34dd11163660839ec
To Van Lam <tovanlam20132223@gmail.com>
2020-11-12 07:14:17 +0700

    add_get_api

--- controllers/tuannv48_api/routes.py
+++ controllers/tuannv48_api/routes.py
@@ -137,6 +137,65 @@ class add_threshold_to_template(Resource):
         session.close()
         return custom_response_flask_restful(request, 201, None, None, res)
 
+
+@namespace.route('/api/get_template', methods=['GET'])
+class get_template(Resource):
+    @namespace.doc(params={'group_id':"",'metricid':"" })
+    @namespace.response(200, '/api/v1/api/get_template?group_id=xxxx&metricid=yyyy')
+    def get(self):
+        if not (request.args.get('group_id') or request.args.get('metricid')):
+            abort(400)
+
+        data = {'group_id':int(request.args.get('group_id')),'metricid':int(request.args.get('metricid')) }
+
+        metricdetail = session.query(models.Metricdetail).filter_by(metricid=data.get('metricid')).first()
+        if metricdetail is None:
+            return custom_response_flask_restful(request, 404, "metricdetail chua ton tai", None, None)
+
+        groupdevice = session.query(models.Groupdevice).filter_by(group_id=data.get('group_id')).first()
+        if groupdevice is None:
+            return custom_response_flask_restful(request, 404, "groupdevice chua ton tai", None, None)
+
+        list_groupdevicepolledatatemplates =session.query(models.Groupevicepolledatatemplate).filter_by(groupdevices_group_id=data.get('group_id')).all()
+        print(list_groupdevicepolledatatemplates)
+        for groupdevicepolledatatemplate in list_groupdevicepolledatatemplates:
+            if groupdevicepolledatatemplate.groupdevices_group_id==data.get('group_id'):
+                polldatatemplate_id = groupdevicepolledatatemplate.polldatatemplate_id
+
+                polldatatemplate_ex=session.query(models.Polldatatemplate).filter_by(id=polldatatemplate_id, metricdetail_id=data.get('metricid') ,active=1).first()
+                if polldatatemplate_ex is not None:
+
+                    threshold_list_id=polldatatemplate_ex.threshold_list_id
+                    threshold_list=session.query(models.Threshold_list).filter_by( id=threshold_list_id).first()
+                    if threshold_list is None:
+                        return custom_response_flask_restful(request, 404, "threshold_list chua ton tai", None,None)
+                    else:
+                        thresholdobjects = threshold_list.thresholdobjects
+                        res_json={
+                            'metricdetail':models.to_json(metricdetail, 'Metricdetail', False),
+                            'groupdevice':models.to_json(groupdevice, 'Groupdevice', False),
+                            'polldatatemplate_ex':models.to_json(polldatatemplate_ex, 'Polldatatemplate', False),
+                            'threshold_list':models.to_json(threshold_list, 'Threshold_list', False),
+                            'thresholdobjects':models.to_json(thresholdobjects, 'Thresholdobject', True)
+                        }
+
+                    session.close()
+                    return custom_response_flask_restful(request, 201, None, 'Ok roi nhe', res_json)
+                else:
+                    res_json = {
+                        'metricdetail': models.to_json(metricdetail, 'Metricdetail', False),
+                        'groupdevice': models.to_json(groupdevice, 'Groupdevice', False),
+                    }
+                    return custom_response_flask_restful(request, 404, "polldatatemplate_ex chua ton tai", None, res_json)
+
+        res_json = {
+            'metricdetail': models.to_json(metricdetail, 'Metricdetail', False),
+            'groupdevice': models.to_json(groupdevice, 'Groupdevice', False),
+        }
+        session.close()
+        return custom_response_flask_restful(request, 201, "Some thing wrong, try again", None, res_json)
+
+
 @namespace.route('/api/update_threshold_in_template', methods=['POST'])
 class update_threshold_in_template(Resource):
     resource_update_threshold_in_template = namespace.model('update_threshold_in_template',
@@ -495,3 +554,43 @@ class delete_threshold_in_device(Resource):
         res = models.to_json(threshold_list, 'Threshold_list', False)
         session.close()
         return custom_response_flask_restful(request, 201, None, 'Ok roi nhe, check in http://127.0.0.1:1234/api/v1/threshold_lists/'+ str(threshold_list.id), res)
+
+
+@namespace.route('/api/get_device_metric', methods=['GET'])
+class get_device_metric(Resource):
+    @namespace.doc(params={'device_id': "", 'metricid': ""})
+    @namespace.response(200, '/api/v1/api/get_device_metric?device_id=xxxx&metricid=yyyy')
+    def get(self):
+        if not (request.args.get('device_id') or request.args.get('metricid')):
+            abort(400)
+
+        data = {'device_id': int(request.args.get('device_id')), 'metricid': int(request.args.get('metricid'))}
+        metricdetail = session.query(models.Metricdetail).filter_by(metricid=data.get('metricid')).first()
+        if metricdetail is None:
+            return custom_response_flask_restful(request, 404, "metricdetail chua ton tai", None, None)
+
+        device = session.query(models.Devicedetail).filter_by(did=data.get('device_id', "")).first()
+        if device is None:
+            return custom_response_flask_restful(request, 404, "device chua ton tai", None, None)
+        polleddata = session.query(models.Polleddata).filter_by(devicedetails_did=device.did, metricdetails_metricid=metricdetail.metricid).first()
+        if polleddata is None:
+            return custom_response_flask_restful(request, 404, "polleddata chua ton tai", None, None)
+
+
+        threshold_list_id = polleddata.threshold_lists_id
+        threshold_list = session.query(models.Threshold_list).filter_by(id=threshold_list_id).first()
+        if threshold_list is None:
+            return custom_response_flask_restful(request, 404, "threshold_list chua ton tai", None, None)
+        else:
+            thresholdobjects = threshold_list.thresholdobjects
+            res_json = {
+                'metricdetail': models.to_json(metricdetail, 'Metricdetail', False),
+                'device': models.to_json(device, 'Devicedetail', False),
+                'polleddata': models.to_json(polleddata, 'Polleddata', False),
+                'threshold_list': models.to_json(threshold_list, 'Threshold_list', False),
+                'thresholdobjects': models.to_json(thresholdobjects, 'Thresholdobject', True)
+            }
+            session.commit()
+
+        session.close()
+        return custom_response_flask_restful(request, 201, None, 'Ok roi nhe', res_json)
